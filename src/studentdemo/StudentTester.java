0f25ae1488d1512d1eece627132eb91b42cb4157
Parminder0079 <sing8209@sheridancollege.ca>
2019-05-22 12:05:36 -0400

    StudentDemo

--- src/studentdemo/StudentTester.java
+++ src/studentdemo/StudentTester.java
@@ -15,16 +15,17 @@ public static void main(String[] args)
 {
        StudentDemo s1 = new StudentDemo();
        s1.setName("sivagama");
+       s1.setSid(123);
        //System.out.println(s1.getName());
-     StudentDemo s2 = new StudentDemo("Daniel");
-StudentDemo s3 = new StudentDemo("Arsh smdnsdmf jknfg" );
+     StudentDemo s2 = new StudentDemo("Daniel",222);
+StudentDemo s3 = new StudentDemo("Arsh smdnsdmf jknfg",333 );
 StudentDemo[] list = new StudentDemo[3];//Array of objects
 list[0]=s1;
 list[1]=s2;
 list[2]=s3;
 for(int i=0;i<list.length;i++)
 {
-System.out.println(list[i].getName());
+System.out.println(list[i].getName() + list[i].getSid());
 
 }
     
